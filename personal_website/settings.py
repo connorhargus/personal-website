9a07529a9d2313d3190141eff1daf545a713678e
Connor Hargus <c.hargus@columbia.edu>
2019-08-20 15:20:26 -0700

    Fix minor static_root issue with production

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Where to store uploaded files
