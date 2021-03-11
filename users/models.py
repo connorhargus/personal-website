7f4e62e34383f030cbccd5a718c8a2d4034d310f
Connor Hargus <c.hargus@columbia.edu>
2021-03-11 14:10:45 -0800

    Remove image cropping feature and update Pillow dependency

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image, ImageOps
+# from PIL import Image, ImageOps
 
 
 class Profile(models.Model):
@@ -10,15 +10,15 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username}\'s Profile'
 
-    # Override save method to crop/shrink photos for efficiency
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-
-            # Crop to a square image without stretching image
-            thumb = ImageOps.fit(img, output_size, Image.ANTIALIAS)
-            thumb.save(self.image.path)
+    # # Override save method to crop/shrink photos for efficiency
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
+    #
+    #     img = Image.open(self.image.path)
+    #
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #
+    #         # Crop to a square image without stretching image
+    #         thumb = ImageOps.fit(img, output_size, Image.ANTIALIAS)
+    #         thumb.save(self.image.path)
